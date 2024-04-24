201e297ea958e7ab3465e7117a2de3db98a1d629
Radka Pavlišová <radojka@seznam.cz>
2024-04-24 15:35:12 +0200

    Filtrování a zobrazení výsledků na stránce

---
+++ script.js
@@ -0,0 +1,31 @@
+const availableKeywords = ["Marie", "Hana", "Jana", "Petra", "Monika", "Tereza"]
+
+const input = document.getElementById("input")
+const resultBox = document.querySelector(".result-box")
+
+const search = () => {
+    let results = []
+    const searchedText = input.value.toLowerCase()
+
+    if (searchedText.length) {
+        results = availableKeywords.filter(oneWord => oneWord.toLowerCase().includes(searchedText)
+        )
+    }
+
+    display(results)
+    if (!results.length) { resultBox.innerHTML = '' }
+
+}
+
+const display = (results) => {
+    const ul = document.createElement('ul')
+    results.forEach(oneItem => {
+        const li = document.createElement('li')
+        li.textContent = oneItem
+        ul.append(li)
+    })
+    resultBox.innerHTML = ''
+    resultBox.append(ul)
+}
+
+input.addEventListener("keyup", search)
